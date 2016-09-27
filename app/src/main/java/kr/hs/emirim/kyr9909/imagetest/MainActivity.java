d25a3cdc60d743ab9a06d382653aa5bc44a6b16b
Yurim Kang <kyr9909@naver.com>
2016-09-27 20:26:04 +0900

    #4 Event Programming

--- app/src/main/java/kr/hs/emirim/kyr9909/imagetest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kyr9909/imagetest/MainActivity.java
@@ -7,19 +7,46 @@ import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyImage myImg;
+    static final int ORIGINAL=0;
+    static  final int ROTATE=1;
+    static final int TRANSLATE=2;
+    static final int SCALE=3;
+    static final int SKEW=4;
+    int choose=ORIGINAL;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyImage(getApplicationContext()));
+        setContentView(R.layout.activity_main);
+        LinearLayout linear1=(LinearLayout)findViewById(R.id.linear1);
+        myImg=new MyImage(getApplicationContext());
+        linear1.addView(myImg);
     }
 
-    class MyImage extends View{
-        MyImage(Context context){
-            super(context);
+    public void transformImage(View v){
+        switch (v.getId()){
+            case R.id.but_rotate:
+                choose=ROTATE;
+                break;
+            case R.id.but_translate:
+                choose=TRANSLATE;
+                break;
+            case R.id.but_scale:
+                choose=SCALE;
+                break;
+            case R.id.but_skew:
+                choose=SKEW;
+                break;
         }
+        myImg.setChoose(choose);
+        myImg.invalidate();
+    }
 
 //        protected void onDraw(Canvas canvas){
 //            super.onDraw(canvas);
@@ -32,5 +59,4 @@ public class MainActivity extends AppCompatActivity {
 //            canvas.translate(-150,200);
 //            canvas.drawBitmap(picture,x,y,null);
 //        }
-    }
 }
--- app/src/main/java/kr/hs/emirim/kyr9909/imagetest/MyImage.java
+++ app/src/main/java/kr/hs/emirim/kyr9909/imagetest/MyImage.java
@@ -10,19 +10,41 @@ import android.view.View;
  * Created by appcreator16 on 2016. 9. 26..
  */
 public class MyImage extends View{
-    public MyImage(Context context) {
+    int choose=MainActivity.ORIGINAL;
+    Bitmap picture;
+
+    MyImage(Context context) {
         super(context);
+        picture= BitmapFactory.decodeResource(getResources(), R.drawable.puppy);
+    }
+    public void setChoose(int choose)
+    {
+        this.choose=choose;
     }
 
     protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
-        Bitmap picture= BitmapFactory.decodeResource(getResources(), R.drawable.puppy);
+
         float cx=getWidth()/2.0f;
         float cy=getHeight()/2.0f;
         float x=(getWidth()-picture.getWidth())/2.0f;
         float y=(getHeight()-picture.getHeight())/2.0f;
-        //canvas.rotate(45,cx,cy); 회전이동
-        canvas.translate(-150,200);
+        switch (choose)
+        {
+            case MainActivity.ROTATE:
+                canvas.rotate(45,cx,cy); //회전
+                break;
+            case MainActivity.TRANSLATE:
+                canvas.translate(-150,200); //이동
+                break;
+            case MainActivity.SCALE:
+                canvas.scale(1.5f,1.5f,cx,cy); //크기
+                break;
+            case MainActivity.SKEW:
+                canvas.skew(0.4f,0.4f); // 비틀기
+                break;
+        }
         canvas.drawBitmap(picture,x,y,null);
+        //picture.recycle();
     }
 }
